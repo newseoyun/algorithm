59ccc3a2fbd4db12b3fbb9f2ec3914f75316dc0f
macSeoyun <newseoyun@gmail.com>
2021-08-01 23:03:31 +0900

    .

---
+++ JAVA 알고리즘 문제풀이/two_pointers_sliding_window/SlidingWindow.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * 연속된 k일의 합이 최대 매출인 그 합 구하기
+ */
+class SlidingWindow {
+
+    int solution(int n, int k, int[] arr) {
+        int answer = 0;
+        int sum = 0;
+        for (int i=0; i<k; i++) {
+            sum += arr[i];
+        }
+        answer = sum;
+        for (int i=k; i<n; i++) {
+            sum += arr[i] - arr[i-k];
+            answer = Math.max(answer, sum);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        SlidingWindow T = new SlidingWindow();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.print(T.solution(n, k, arr));
+    }
+}
