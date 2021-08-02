66f3d9f38925c5a6f8432e1dc727212831991ab3
macSeoyun <newseoyun@gmail.com>
2021-08-02 22:48:22 +0900

    .

--- JAVA 알고리즘 문제풀이/two_pointers_sliding_window/complex04.java
+++ JAVA 알고리즘 문제풀이/two_pointers_sliding_window/complex04.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
 
 /**
  * 연속 부분 수열(복합적 문제)
  */
-public class complex04 {
+public class Complex04 {
     
+    int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        int sum = 0;
+        int lt = 0;
+        for (int rt=0; rt<n; rt++) {
+            sum += arr[rt];
+            if (sum == m) answer++;
+            while (sum >= m) {
+                sum -= arr[lt++];
+                if (sum == m) answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Complex04 T = new Complex04();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.print(T.solution(n, m, arr));
+    }
 }
