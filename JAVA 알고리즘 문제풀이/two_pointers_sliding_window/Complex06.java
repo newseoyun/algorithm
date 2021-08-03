51c6e4adfeef62d7a051ae77a8067178de69fefb
macSeoyun <newseoyun@gmail.com>
2021-08-03 19:36:06 +0900

    .

---
+++ JAVA 알고리즘 문제풀이/two_pointers_sliding_window/Complex06.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+/**
+ * 최대 길이 연속 부분 수열
+ */
+public class Complex06 {
+    
+    int solution(int n, int k, int[] arr) {
+        int answer = 0;
+        int cnt = 0;
+        int lt = 0;
+
+        for (int rt=0; rt<n; rt++) {
+            if (arr[rt] == 0) cnt++;
+            while (cnt > k) {
+                if (arr[lt] == 0) cnt--;
+                lt++;
+            }
+            answer = Math.max(answer, rt);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Complex06 T = new Complex06();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int[] arr = new int[n];
+
+        for (int i=0; i<n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.print(T.solution(n, k, arr));
+    }
+}
