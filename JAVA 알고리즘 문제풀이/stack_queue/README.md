ac1c5a1ef6ab86498aa9eb7b4d65e1216c3409a9
macSeoyun <newseoyun@gmail.com>
2021-08-07 23:22:10 +0900

    .

--- JAVA 알고리즘 문제풀이/stack_queue/README.md
+++ JAVA 알고리즘 문제풀이/stack_queue/README.md
@@ -1,4 +1,9 @@
 ## Stack   
-* 괄호처럼 짝을 판별하는 문제는 주로 Stack 자료구조를 씀.   
+##### 괄호같이 짝을 판별하는 문제는 주로 Stack 자료구조를 씀.   
+##### Truble shooting   
+* pop 하면서 출력도 하므로 while(stack.pop() != '('); 한줄로도 while처리 충분함.
+while(stack.pop() != '('){ stack.pop(); } 하면 팝을 두 번 하는 것.
+
+***
 
 ## Queue
--- JAVA 알고리즘 문제풀이/stack_queue/StackQueue02.java
+++ JAVA 알고리즘 문제풀이/stack_queue/StackQueue02.java
@@ -1,23 +1,31 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  * 괄호 문자 제거
  */
 public class StackQueue02 {
     
-    String solution() {
-
-
-        return "";
+    String solution(String str) {
+        String answer = "";
+        Stack<Character> stack = new Stack<>();
+        for(char x : str.toCharArray()) {
+            if(x == ')') {
+                while(stack.pop() != '(');
+            } else {
+                stack.push(x);
+            }
+        }
+        for(int i=0; i<stack.size(); i++) {
+            answer += stack.get(i);
+        }
+        return answer;
     }
-
 
     public static void main(String[] args) {
         StackQueue02 T = new StackQueue02();
         Scanner kb = new Scanner(System.in);
-        
-
-
-
+        String str = kb.next();
+        System.out.print(T.solution(str));
     }
 }
