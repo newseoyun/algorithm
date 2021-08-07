acd08049b0a35756332e0513f96a159c79edcd8f
macSeoyun <newseoyun@gmail.com>
2021-08-07 22:43:57 +0900

    .

--- JAVA 알고리즘 문제풀이/stack_queue/StackQueue01.java
+++ JAVA 알고리즘 문제풀이/stack_queue/StackQueue01.java
@@ -1,20 +1,28 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  * 올바른 괄호
  */
 public class StackQueue01 {
 
-    String solution(String a) {
-
+    String solution(String str) {
+        Stack<Character> stack = new Stack<>();
+        for(char a : str.toCharArray()) {
+            if(a == '(') stack.push(a);
+            else {
+                if(stack.isEmpty()) return "NO";
+                stack.pop();
+            }
+        }
+        if(!stack.isEmpty()) return "NO";
+        return "YES";
     }
 
-    
     public static void main(String[] args) {
         StackQueue01 T = new StackQueue01();
         Scanner kb = new Scanner(System.in);
-        
-        
+        String str = kb.next();
+        System.out.print(T.solution(str));
     }
-    
 }
