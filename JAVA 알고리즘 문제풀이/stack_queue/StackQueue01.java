3110aded8dc0f6555087b5c1e33d568a455dc339
macSeoyun <newseoyun@gmail.com>
2021-08-07 22:27:49 +0900

    .

--- JAVA 알고리즘 문제풀이/stack_queue/StackQueue01.java
+++ JAVA 알고리즘 문제풀이/stack_queue/StackQueue01.java
@@ -1,14 +1,19 @@
-
+import java.util.Scanner;
 
 /**
  * 올바른 괄호
  */
 public class StackQueue01 {
 
+    String solution(String a) {
 
+    }
 
     
     public static void main(String[] args) {
+        StackQueue01 T = new StackQueue01();
+        Scanner kb = new Scanner(System.in);
+        
         
     }
     
