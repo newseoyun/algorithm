621c1f88cd6ee33dfe677331b38db077e4a2ec35
macSeoyun <newseoyun@gmail.com>
2021-08-09 22:30:55 +0900

    .

--- JAVA 알고리즘 문제풀이/06_sorting_searching/SortingSearching01.java
+++ JAVA 알고리즘 문제풀이/06_sorting_searching/SortingSearching01.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
 
 /**
- * 
+ * 선택정렬
  */
 public class SortingSearching01 {
     
+    int[] solution(int n, int[] arr) {
+        for(int i=0; i<n-1; i++) {
+            int idx = i;
+            for(int j=i+1; j<n; j++) {
+                if(arr[j] < arr[idx]) idx = j;
+            }
+            int tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        SortingSearching01 T = new SortingSearching01();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) arr[i] = kb.nextInt();
+        for(int x : T.solution(n, arr)) System.out.print(x + " ");
+    }
 }
