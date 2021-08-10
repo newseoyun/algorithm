4402a8daa6b57b3cef7f573ad511d584b542ba0e
newseoyun <newseoyun@gmail.com>
2021-08-10 12:22:48 +0900

    .

--- JAVA 알고리즘 문제풀이/05_stack_queue/README.md
+++ JAVA 알고리즘 문제풀이/05_stack_queue/README.md
@@ -11,9 +11,9 @@
 ***
 
 ### Stack   
-##### 괄호같이 짝을 판별하는 문제는 주로 Stack 자료구조를    씀.   
+##### 괄호같이 짝을 판별하는 문제는 주로 Stack 자료구조를 씀.   
 ##### Truble shooting   
-* pop 하면서 출력도 하므로 while(stack.pop() != '('); 한줄로도 while처리 충분함.
+* pop 하면서 출력도 하므로 while(stack.pop() != '('); 한줄로도 while처리 충분함.   
 while(stack.pop() != '('){ stack.pop(); } 하면 팝을 두 번 하는 것.
 
 ***
--- JAVA 알고리즘 문제풀이/06_sorting_searching/SortingSearching02.java
+++ JAVA 알고리즘 문제풀이/06_sorting_searching/SortingSearching02.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
 
 /**
- * 
+ * 버블정렬
  */
 public class SortingSearching02 {
     
+    int[] solution(int n, int[] arr) {
+        for(int i=0; i<n-1; i++) {
+            for(int j=0; j<n-i-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        SortingSearching02 T = new SortingSearching02();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) arr[i] = kb.nextInt();
+        for(int a : T.solution(n, arr)) System.out.print(a + " ");
+    }
 }
