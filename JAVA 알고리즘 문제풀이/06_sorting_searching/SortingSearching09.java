b2f084b1a059c1019c2e8b5f348291f8c79d0214
newseoyun <newseoyun@gmail.com>
2021-08-12 16:03:39 +0900

    임시저장

--- JAVA 알고리즘 문제풀이/06_sorting_searching/SortingSearching09.java
+++ JAVA 알고리즘 문제풀이/06_sorting_searching/SortingSearching09.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
 
 /**
- * 
+ * 뮤직비디오(결정알고리즘)
  */
 public class SortingSearching09 {
     
+    int solution(int n, int x, int[] arr) {
+
+
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        SortingSearching09 T = new SortingSearching09();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int arr
+
+
+    }
 }
