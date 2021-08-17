5f3dbcd52b5f843d6c53ff52ecd1311135ac0f40
seoyun <newseoyun@gmail.com>
2021-08-17 21:29:02 +0900

    .

--- JAVA 알고리즘 문제풀이/07_recursive_tree_graph/Rtg01.java
+++ JAVA 알고리즘 문제풀이/07_recursive_tree_graph/Rtg01.java
@@ -5,9 +5,10 @@ public class Rtg01 {
 
     void DFS(int n) {
         if (n == 0) return;
-        
-        System.out.println("n: " + n);
+
+        System.out.println("n: " + n); // 3 2 1
         DFS(n-1);
+        System.out.println("n: " + n); // 1 2 3 
     }
 
     public static void main(String[] args) {
---
+++ JAVA 알고리즘 문제풀이/07_recursive_tree_graph/Rtg02.java
@@ -0,0 +1,11 @@
+
+/**
+ * 이진수 출력(재귀)
+ */
+public class Rtg02 {
+    
+
+    public static void main(String[] args) {
+        Rtg02 T = new Rtg02();
+    }
+}
