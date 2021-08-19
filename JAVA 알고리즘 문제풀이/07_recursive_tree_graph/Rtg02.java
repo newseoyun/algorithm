36216f175b3a8afe9eabb7cbd9723e52f97f2530
seoyun <newseoyun@gmail.com>
2021-08-19 21:16:15 +0900

    .

--- JAVA 알고리즘 문제풀이/07_recursive_tree_graph/Rtg02.java
+++ JAVA 알고리즘 문제풀이/07_recursive_tree_graph/Rtg02.java
@@ -4,8 +4,14 @@
  */
 public class Rtg02 {
     
+    void DFS(int n) {
+        if (n == 0) return;
+        DFS(n/2);
+        System.out.print(n%2);
+    }
 
     public static void main(String[] args) {
         Rtg02 T = new Rtg02();
+        T.DFS(11);
     }
 }
