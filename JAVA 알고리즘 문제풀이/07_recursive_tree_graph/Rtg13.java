15cfc190138accdcd29d89da48c8b20ebdd13c91
seoyun <newseoyun@gmail.com>
2021-09-01 20:35:46 +0900

    DFS

---
+++ JAVA 알고리즘 문제풀이/07_recursive_tree_graph/Rtg13.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * 경로탐색(인접리스트, ArrayList)
+ */
+public class Rtg13 {
+
+    static int n = 0;
+    static int m = 0;
+    static int answer = 0;
+    static ArrayList<ArrayList<Integer>> graph;
+    static int[] checkArray;
+
+    void DFS(int v) {
+        if (v == n) answer++;
+        else {
+            for (int nv : graph.get(v)) {
+                if (checkArray[nv] == 0) {
+                    checkArray[nv] = 1;
+                    DFS(nv);
+                    checkArray[nv] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Rtg13 T = new Rtg13();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        m = kb.nextInt();
+        graph = new ArrayList<ArrayList<Integer>>();
+        for (int i=0; i<=n; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+        checkArray = new int[n+1];
+        for (int i=0; i<m; i++) {
+            int a = kb.nextInt();
+            int b = kb.nextInt();
+            graph.get(a).add(b);
+        }
+        checkArray[1] = 1;
+        T.DFS(1);
+        System.out.println(answer);
+    }
+    
+}
