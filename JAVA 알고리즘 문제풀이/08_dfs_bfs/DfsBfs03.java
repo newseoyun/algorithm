df8634eefa3d866a6a33a3411b4be23e2933f2e0
seoyun <newseoyun@gmail.com>
2021-09-07 23:51:59 +0900

    DFS

---
+++ JAVA 알고리즘 문제풀이/08_dfs_bfs/DfsBfs03.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ * 최대점수 구하기
+ */
+public class DfsBfs03 {
+    static int answer = Integer.MIN_VALUE;
+    static int n;
+    static int m; // 제한시간
+
+    boolean flag = false;
+
+    void DFS(int L, int sum, int time, int[] ps, int[] pt) {
+        if (time > m) return;
+        if (L == n) {
+            answer = Math.max(answer, sum);
+        } else {
+            DFS(L + 1, sum + ps[L], time + pt[L], ps, pt);
+            DFS(L + 1, sum, time, ps, pt);
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        DfsBfs03 T = new DfsBfs03();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        m = kb.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+        for (int i=0; i<n; i++) {
+            a[i] = kb.nextInt();
+            b[i] = kb.nextInt();
+        }
+        T.DFS(0, 0, 0, a, b);
+        System.out.println(answer);
+    }
+}
