bfd8329c00fba89adcabc23422317c683dd9d1fb
macSeoyun <newseoyun@gmail.com>
2021-09-14 23:57:35 +0900

    DFS

---
+++ JAVA 알고리즘 문제풀이/08_dfs_bfs/DfsBfs06.java
@@ -0,0 +1,48 @@
+
+/**
+ * 순열 구하기
+ * 
+입력 (숫자 3개. 2열)
+3 2
+3 6 9
+
+출력
+3 6
+3 9
+6 3
+6 9
+9 3
+9 6
+
+ */
+public class DfsBfs06 {
+
+    static int[] pm;
+    static int[] ch;
+    static int[] arr;
+    static int n;
+    static int m;
+
+    void DFS(int L) {
+        if (L == m) {
+            for (int x : pm) System.out.print(x + " ");
+            System.out.println();
+        } else {
+            for (int i=0; i<n; i++) {
+                if (ch[i] == 0) {
+                    ch[i] = 1;
+                    pm[L] = arr[i];
+                    DFS(L + 1);
+                    ch[i] = 0;
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        DfsBfs06 T = new DfsBfs06();
+        Scanner kb = new Scanner(System.in);
+        
+    }
+    
+}
