65de99d89c5229b33eb7dcf99a350ce75d0fece3
seoyun <newseoyun@gmail.com>
2021-09-19 21:55:51 +0900

    greedy

--- JAVA 알고리즘 문제풀이/09_greedy/Greedy01.java
+++ JAVA 알고리즘 문제풀이/09_greedy/Greedy01.java
@@ -1,39 +1,49 @@
 import java.util.*;
 
-class Body implements Comparable<Body>{
-    public int h, w;
-    Body(int h, int w) {
+/**
+ * 씨름선수
+ */
+
+class Body implements Comparable<Body> {
+    public int h;
+    public int w;
+
+    Body(int h, int w)  {
         this.h = h;
         this.w = w;
     }
+
     @Override
-    public int compareTo(Body o){
-        return o.h-this.h;
+    public int compareTo(Body o) {
+        return o.h - this.h; // h 오름차순
     }
 }
 
 class Greedy01 {
-    public int solution(ArrayList<Body> arr, int n){
-        int cnt=0;
+    public int solution(ArrayList<Body> arr, int n) {
+        int cnt = 0;
         Collections.sort(arr);
-        int max=Integer.MIN_VALUE;
-        for(Body ob : arr){
-            if(ob.w>max){
-                max=ob.w;
+        int max = Integer.MIN_VALUE;
+
+        for (Body ob : arr) {
+            if (ob.w > max) {
+                max = ob.w;
                 cnt++;
             }
         }
         return cnt;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Greedy01 T = new Greedy01();
         Scanner kb = new Scanner(System.in);
-        int n=kb.nextInt();
+        int n = kb.nextInt();
+        
         ArrayList<Body> arr = new ArrayList<>();
-        for(int i=0; i<n; i++){
-            int h=kb.nextInt();
-            int w=kb.nextInt();
+
+        for (int i=0; i<n; i++) {
+            int h = kb.nextInt();
+            int w = kb.nextInt();
             arr.add(new Body(h, w));
         }
         System.out.println(T.solution(arr, n));
---
+++ JAVA 알고리즘 문제풀이/09_greedy/Greedy03.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+/**
+ * 결혼식
+ */
+
+class TimeS implements Comparable<TimeS> {
+    public int time;
+	public char state;
+
+    TimeS(int time, char state) {
+        this.time = time;
+        this.state = state;
+    }
+
+    @Override
+    public int compareTo(TimeS ob) {
+        if(this.time == ob.time) return this.state - ob.state;
+		else return this.time - ob.time;
+    }
+}
+
+class Greedy03 {
+    public int solution(ArrayList<TimeS> arr) {
+        int answer = Integer.MIN_VALUE;
+        Collections.sort(arr);
+        int cnt = 0;
+
+        for (TimeS ob : arr) {
+            if (ob.state == 's') cnt++;
+            else cnt--;
+
+            answer = Math.max(answer, cnt);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Greedy03 T = new Greedy03();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+
+        ArrayList<TimeS> arr = new ArrayList<>();
+
+        for (int i=0; i<n; i++) {
+            int sT = kb.nextInt();
+            int eT = kb.nextInt();
+            arr.add(new TimeS(sT, 's'));
+            arr.add(new TimeS(eT, 'e'));
+        }
+        System.out.println(T.solution(arr));
+    }
+}
