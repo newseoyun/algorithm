7d7bd08b5b554423c093ef28a004c025cac7ecad
seoyun <newseoyun@gmail.com>
2021-09-19 23:26:13 +0900

    greedy

---
+++ JAVA 알고리즘 문제풀이/09_greedy/Greedy07.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+/**
+ * 원더랜드(크루스칼)
+ */
+
+class Edge07 implements Comparable<Edge07> {
+    public int vertex1;
+    public int vertex2;
+    public int cost;
+
+    Edge07(int vertex1, int vertex2, int cost) {
+        this.vertex1 = vertex1;
+        this.vertex2 = vertex2;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Edge07 ob) {
+        return this.cost - ob.cost;
+    }
+}
+
+class Greedy07 {
+    static int[] unf;
+
+    public static int Find(int v) {
+        if (v == unf[v]) return v;
+        else return unf[v] = Find(unf[v]);
+    }
+
+    public static void Union(int a, int b) {
+        int fa = Find(a);
+        int fb = Find(b);
+        
+        if (fa != fb) unf[fa] = fb;
+    }
+    
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        unf = new int[n + 1];
+        ArrayList<Edge07> arr = new ArrayList<>();
+
+        for (int i=1; i<=n; i++) unf[i] = i;
+
+        for (int i=0; i<m; i++) {
+            int a = kb.nextInt();
+            int b = kb.nextInt();
+            int c = kb.nextInt();
+            arr.add(new Edge07(a, b, c));
+        }
+        int answer = 0;
+        Collections.sort(arr);
+
+        for (Edge07 ob : arr) {
+            int fvertex1 = Find(ob.vertex1);
+            int fvertex2 = Find(ob.vertex2);
+
+            if (fvertex1 != fvertex2) {
+                answer += ob.cost;
+                Union(ob.vertex1, ob.vertex2);
+            }
+        }
+        System.out.println(answer);
+    }
+}
---
+++ JAVA 알고리즘 문제풀이/09_greedy/Greedy08.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+/**
+ * 원더랜드(프림)
+ */
+
+class Edge08 implements Comparable<Edge08> {
+    public int vertex;
+	public int cost;
+
+    Edge08(int vertex, int cost) {
+        this.vertex = vertex;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Edge08 ob) {
+        return this.cost - ob.cost;
+    }
+}
+
+class Greedy08 {
+
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		int m = kb.nextInt();
+		ArrayList<ArrayList<Edge08>> graph = new ArrayList<ArrayList<Edge08>>();
+
+		for (int i=0; i<=n; i++) {
+			graph.add(new ArrayList<Edge08>());
+		}
+		int[] ch=new int[n + 1];
+
+		for (int i=0; i<m; i++) {
+			int a = kb.nextInt();
+			int b = kb.nextInt();
+			int c = kb.nextInt();
+			graph.get(a).add(new Edge08(b, c));
+			graph.get(b).add(new Edge08(a, c));
+		}
+		int answer = 0;
+		PriorityQueue<Edge08> pQ = new PriorityQueue<>();
+		pQ.offer(new Edge08(1, 0));
+
+		while (!pQ.isEmpty()) {
+			Edge08 tmp = pQ.poll();
+			int ev = tmp.vertex;
+
+			if (ch[ev] == 0) {
+				ch[ev] = 1;
+				answer += tmp.cost;
+
+				for (Edge08 ob : graph.get(ev)) {
+					if (ch[ob.vertex] == 0) pQ.offer(new Edge08(ob.vertex, ob.cost));
+				}
+			}
+		}
+		System.out.println(answer);
+	}
+}
