e4e0cc939faf124194625b51d2bfc76662e1d27a
seoyun <newseoyun@gmail.com>
2021-09-19 23:50:39 +0900

    fix

--- JAVA 알고리즘 문제풀이/10_dynamic/Dynamic01.java
+++ JAVA 알고리즘 문제풀이/10_dynamic/Dynamic01.java
@@ -3,20 +3,21 @@ import java.util.*;
 /**
  * 계단 오르기
  */
-class Dynamic01{
-	static int[] dy;
-	public int solution(int n){
-		dy[1]=1;
-		dy[2]=2;
-		for(int i=3; i<=n; i++) dy[i]=dy[i-2]+dy[i-1];
-		return dy[n];
-	}
+class Dynamic01 {
+    static int[] dy;
 
-	public static void main(String[] args){
-		Dynamic01 T = new Dynamic01();
-		Scanner kb = new Scanner(System.in);
-		int n=kb.nextInt();
-		dy=new int[n+1];
-		System.out.print(T.solution(n));
-	}
+    public int solution(int n) {
+        dy[1] = 1;
+        dy[2] = 2;
+        for (int i=3; i<=n; i++) dy[i] = dy[i - 2] + dy[i - 1];
+        return dy[n];
+    }
+
+    public static void main(String[] args) {
+        Dynamic01 T = new Dynamic01();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        dy = new int[n + 1];
+        System.out.print(T.solution(n));
+    }
 }
--- JAVA 알고리즘 문제풀이/10_dynamic/Dynamic06.java
+++ JAVA 알고리즘 문제풀이/10_dynamic/Dynamic06.java
@@ -4,18 +4,20 @@ import java.util.*;
  * 최대 점수 구하기(냅색 알고리즘)
  */
 class Dynamic06{
-	public static void main(String[] args){
-		Scanner kb = new Scanner(System.in);
-		int n=kb.nextInt();
-		int m=kb.nextInt();
-		int[] dy=new int[m+1];
-		for(int i=0; i<n; i++){
-			int ps=kb.nextInt();
-			int pt=kb.nextInt();
-			for(int j=m; j>=pt; j--){
-				dy[j]=Math.max(dy[j], dy[j-pt]+ps);
-			}
-		}
-		System.out.print(dy[m]);
-	}
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int[] dy = new int[m + 1];
+
+        for (int i=0; i<n; i++) {
+            int ps = kb.nextInt();
+            int pt = kb.nextInt();
+            for (int j=m; j>=pt; j--) {
+                dy[j] = Math.max(dy[j], dy[j - pt] + ps);
+            }
+        }
+        System.out.print(dy[m]);
+    }
 }
