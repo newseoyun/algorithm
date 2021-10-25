65b5652b4ebd53828f68ad1ab9bc8732f43a4668
macSeoyun <newseoyun@gmail.com>
2021-10-25 21:23:24 +0900

    .

--- baekjoon/for/10950/Main.java
+++ baekjoon/for/10950/Main.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
+
 class Main {
-    
     public static void main(String args[]) {
-
-
-
-        
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        int[] arr = new int[len*2];
+        for (int i=0; i<arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+        for (int j=0; j<arr.length; j=j+2) {
+            System.out.println(arr[j] + arr[j+1]);
+        }
     }
+
+    /*
+
+		Scanner sc=new Scanner(System.in);
+		int c=sc.nextInt();
+		
+		for(int i=0;i<c;i++) {
+			
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			System.out.println(a+b);
+		}
+
+    */
+
+
 }
