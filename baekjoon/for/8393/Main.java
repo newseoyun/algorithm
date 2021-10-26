620a52c132aa1f5a83676618aee4e7fe50bf6e3c
macSeoyun <newseoyun@gmail.com>
2021-10-26 23:44:32 +0900

    test

--- baekjoon/for/8393/Main.java
+++ baekjoon/for/8393/Main.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
@@ -9,9 +11,17 @@ class Main {
 
     public static void main(String args[]) {
 
-        Scanner sc = new Scanner(System.in);
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int test;
+        try {
+            test = Integer.parseInt(br.readLine());
+            br.close();
+            System.out.println("test? " + test);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
     }
     
