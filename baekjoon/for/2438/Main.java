46729a35eca431d2ef0b0b345a8d6de90335aaf3
seoyun <newseoyun@gmail.com>
2021-11-05 19:01:24 +0900

    .

---
+++ baekjoon/for/2438/Main.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * 
+ */
+class Main {
+    
+    public static void main(String args[]) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+        for (int i=0; i)
+    }
+}
